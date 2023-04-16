a7e5a02057ffc2c03838a1940d4c950bb055dd71
Matthew Barnes <011mbarnes@gmail.com>
2023-04-16 16:00:15 +0000

    writing logs to file on mac

--- src/logger/load.rs
+++ src/logger/load.rs
@@ -1,5 +1,4 @@
 use std::path::{Path, PathBuf};
-
 use directories::{BaseDirs};
 use log::LevelFilter;
 use log4rs::{
@@ -25,39 +24,50 @@ use crate::logger::info::InfoLogging;
 use super::structs::{Logger, LoggerError};
 
 impl Logger {
-    fn log_path_if_linux() -> Option<PathBuf> {
+    fn log_path_if_linux_or_mac() -> Option<PathBuf> {
         let os = std::env::consts::OS;
 
-        let home_dir = BaseDirs::new().and_then(|p| Some(p.home_dir().to_path_buf()))?.canonicalize().ok()?;
-        let rusty_hooks_log_subdir = Path::new("rusty-hooks/logs/rusty-hooks.log").to_path_buf();
-
-        let log_path_vec:PathBuf = [
-            home_dir.as_path(),
-            rusty_hooks_log_subdir.as_path()
-        ].iter().collect();
-
         match os {
-            "linux" => Some(log_path_vec),
+            "linux" => Self::linux_log_path(),
+            "mac" => Self::mac_log_path(),
             _ => None
         }
     }
 
+    fn linux_log_path() -> Option<PathBuf> {
+        let home_dir = BaseDirs::new().and_then(|p| Some(p.home_dir().to_path_buf()))?.canonicalize().ok()?;
+        let rusty_hooks_log_subdir = Path::new("rusty-hooks/logs/rusty-hooks.log").to_path_buf();
+        Some([
+            home_dir.as_path(),
+            rusty_hooks_log_subdir.as_path()
+        ].iter().collect())
+    }
+    
+    fn mac_log_path() -> Option<PathBuf> {
+        let home_dir = BaseDirs::new().and_then(|p| Some(p.home_dir().to_path_buf()))?.canonicalize().ok()?;
+        let rusty_hooks_log_subdir = Path::new("Library/rusty-hooks/logs/rusty-hooks.log").to_path_buf();
+        Some([
+            home_dir.as_path(),
+            rusty_hooks_log_subdir.as_path()
+        ].iter().collect())
+    }
+
     pub fn on_load(level: LevelFilter) -> Result<log4rs::Handle, LoggerError> {
-        let file_path = Self::log_path_if_linux();
+        let file_path = Self::log_path_if_linux_or_mac();
         // Build a stderr logger.
         let stderr = ConsoleAppender::builder().target(Target::Stderr).build();
         let stderr_appender = Appender::builder()
-        .filter(Box::new(ThresholdFilter::new(level)))
-        .build("stderr", Box::new(stderr));
+            .filter(Box::new(ThresholdFilter::new(level)))
+            .build("stderr", Box::new(stderr));
     
-    match file_path {
-        Some(path) => {
+        match file_path {
+            Some(path) => {
                 let path_clone = path.clone();
                 let file_path_str = path_clone.to_str().unwrap_or("whatever");
                 let window_size = 3; // log0, log1, log2
                 let fixed_window_roller = FixedWindowRoller::builder().build("log{}",window_size).unwrap();
         
-                let size_limit = 5 * 1024; // 5KB as max log file size to roll
+                let size_limit = 5000 * 1024; // 5MB as max log file size to roll
                 let size_trigger = SizeTrigger::new(size_limit);
         
                 let compound_policy = CompoundPolicy::new(Box::new(size_trigger),Box::new(fixed_window_roller));
@@ -84,7 +94,7 @@ impl Logger {
                 Ok(log4rs::init_config(config)?)
             },
             None => {
-                // Only logging to stderr if system isn't mac or linux
+                // Only logging to stderr if system isn't linux
                 let config = Config::builder()
                     .appender(stderr_appender)
                     .build(
