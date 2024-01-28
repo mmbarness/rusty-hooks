e58835f6241a2a9841d742f04747e238fa62a49e
Matthew Barnes <011mbarnes@gmail.com>
2024-01-28 19:56:11 +0000

    update regexes

--- tests/common/stdout_strs.rs
+++ tests/common/stdout_strs.rs
@@ -6,6 +6,6 @@ Usage: rusty-hooks --script-folder <SCRIPT_FOLDER>
 
 For more information, try \'--help\'.
 ";
-pub const INVALID_SCRIPT_FOLDER_REGEX:&str = r#"\n(?<timestamp>ts=(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}\.[0-9]*)\+(\d{2}:\d{2})) (?<debug_level>level=(INFO|TRACE|ERROR|DEBUG)) (?<message>message="io error while parsing command line args: `No such file or directory \(os error 2\)`") (?<src>src=.*) (?<pid>pid=[0-9]*)"#;
-pub const LOGGING_REGEX:&str = r#"\n(?<timestamp>ts=(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}\.[0-9]*)\+(\d{2}:\d{2})) (?<debug_level>level=(INFO|TRACE|ERROR|DEBUG)) (?<message>message=".*") (?<src>src=.*) (?<pid>pid=[0-9]*)"#;
-pub const WATCH_PATH_REGEX:&str = r#"\n(?<timestamp>ts=(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}\.[0-9]*)\+(\d{2}:\d{2})) (?<debug_level>level=(INFO|TRACE|ERROR|DEBUG)) (?<message>message="now watching path: \/media\/wd3\/programming\/rusty-hooks\/tests\/files\/watch_location") (?<src>src=.*) (?<pid>pid=[0-9]*)"#;
+pub const INVALID_SCRIPT_FOLDER_REGEX:&str = r#"\n(?<debug_level>level=(INFO|TRACE|ERROR|DEBUG)) | (?<message>message="io error while parsing command line args: `No such file or directory \(os error 2\)`") | (?<src>src=.*)"#;
+pub const LOGGING_REGEX:&str = r#"\n(?<debug_level>level=(INFO|TRACE|ERROR|DEBUG)) | (?<message>message=".*") | (?<src>src=.*)"#;
+pub const WATCH_PATH_REGEX:&str = r#"\n(?<debug_level>level=(INFO|TRACE|ERROR|DEBUG)) | (?<message>message="now watching path: \/media\/wd3\/programming\/rusty-hooks\/tests\/files\/watch_location") | (?<src>src=.*)"#;
